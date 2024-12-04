1c903754ebb36e0c8f202700a37379d03525e774
Dauda-abba <fwenshinen1@gmail.com>
2024-12-05 00:11:46 +0100

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+Dictionary = {
+    'zo':'come',
+    'je':'go',
+    'kafa':'leg',
+    'hanu':'hand',
+    'kai':'head',
+    'suma':'hair',
+    'idanu':'eyes',
+    'kune':'ear',
+    'hanci':'nose',
+    'harse':'tongue'
+}
+
+word = input('enter an hausa word: ')
+print(Dictionary[word])
